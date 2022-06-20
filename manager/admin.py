eb0f859cd918ec4be1c171748c96793d53800ee8
erikandersson96 <erik96.andersson@gmail.com>
2022-06-20 11:55:13 +0000

    add so admin can see and add recipe

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    """
+    Admin use summernote for recipe
+    """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('method', 'ingredients')
+    readonly_fields = ('slug',)
+  

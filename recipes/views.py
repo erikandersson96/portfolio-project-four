b2b28b471267c78c9415d8bcb843b5034dd2357d
erikandersson96 <erik96.andersson@gmail.com>
2022-06-21 16:06:23 +0000

    Add section with if user authent to home html

--- recipes/views.py
+++ recipes/views.py
@@ -10,5 +10,5 @@ class RecipeListView(generic.ListView):
     """
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name = 'base.html'
+    template_name = 'home.html'
     paginate_by = 6

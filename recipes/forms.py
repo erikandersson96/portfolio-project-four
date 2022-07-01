e8c426244d6cf404e2e732fe94df0101440e0d04
erikandersson96 <erik96.andersson@gmail.com>
2022-07-01 21:39:15 +0000

    Add comment in forms

---
+++ recipes/forms.py
@@ -0,0 +1,1 @@
+# Add the form here for RecipeForm!
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
+from .forms import RecipeForm
 
 
 class RecipeListView(generic.ListView):

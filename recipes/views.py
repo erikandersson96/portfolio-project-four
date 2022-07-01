bb21c2e64cf8635b3eefe4e8031de4b5ce64bf4f
erikandersson96 <erik96.andersson@gmail.com>
2022-07-01 21:36:44 +0000

    Add recipe detail to views

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -12,3 +12,24 @@ class RecipeListView(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'home.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+    """
+    Class for a single recipe view
+    """
+    def get(self, request, slug, *args, **kwargs):
+        """
+        Creates the view for a single specific recipe
+        """
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "recipe_form": RecipeForm(),
+            },
+        )

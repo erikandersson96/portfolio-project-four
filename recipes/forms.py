f612068d6879c143b8d37af1b2a2a6e143393b53
erikandersson96 <erik96.andersson@gmail.com>
2022-07-02 11:50:09 +0000

    Add recipeform to forms

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,1 +1,30 @@
-# Add the form for add recipe here!
+from django import forms
+from .models import Recipe
+from django_summernote.widgets import SummernoteInplaceWidget
+
+
+class RecipeForm(forms.ModelForm):
+    """
+    Form for adding a recipe
+    """
+    class Meta:
+        model = Recipe
+        fields = [
+            'title',
+            'excerpt',
+            'difficulty',
+            'serves',
+            'prep_time',
+            'cook_time',
+            'ingredients',
+            'instructions',
+            'meal_image',
+        ]
+
+        widgets = {
+            'ingredients': SummernoteInplaceWidget(),
+            'instructions': SummernoteInplaceWidget()
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)

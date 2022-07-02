01989d97097c965148f2a41ee46014c960be0d35
erikandersson96 <erik96.andersson@gmail.com>
2022-07-02 12:14:13 +0000

    Add path for delete_recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.RecipeListView.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('edit-recipe/<slug:slug>', views.edit_recipe, name='edit_recipe'),
+    path('delete-recipe/<slug:slug>', views.delete_recipe, name='delete_recipe'),
 ]

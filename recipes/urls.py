e456df25735d88875bd281b2ee3fbddc7b2741de
erikandersson96 <erik96.andersson@gmail.com>
2022-07-03 10:47:44 +0000

    Add link for add_recipe to navbar

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name='home'),
+    path('add-recipe/', views.add_recipe, name='add_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('edit-recipe/<slug:slug>', views.edit_recipe, name='edit_recipe'),
     path('delete-recipe/<slug:slug>', views.delete_recipe, name='delete_recipe'),
-    path('add-recipe/', views.add_recipe, name='add_recipe'),
 ]

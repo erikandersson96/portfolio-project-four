296f833fb415c7b59501d06469627a8700f54066
erikandersson96 <erik96.andersson@gmail.com>
2022-07-13 09:12:52 +0000

    change background image src to cloudinary

--- whattoeat/settings.py
+++ whattoeat/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = "DEBUG" in os.environ
 
 ALLOWED_HOSTS = ['whattoeat2022.herokuapp.com', 'localhost']
 

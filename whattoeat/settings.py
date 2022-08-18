2cd0ae9fd2d8398fb77373436cb844005e882116
erikandersson96 <erik96.andersson@gmail.com>
2022-08-18 11:31:16 +0000

    add two blanklines to noqa comment

--- whattoeat/settings.py
+++ whattoeat/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
-    import env # noqa
+    import env  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

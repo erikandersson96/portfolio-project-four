43dd70c71e479dea4b11f681030c70ea1de64373
erikandersson96 <erik96.andersson@gmail.com>
2022-08-24 17:08:39 +0000

    add class for favorite admin admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Favorite
 
-# Register your models here.
+
+@admin.register(Favorite)
+class FavoritesAdmin(admin.ModelAdmin):
+    """
+    Admin for favorite recipes
+    """
+    list_display = ('user', 'favorite_recipe')

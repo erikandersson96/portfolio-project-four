33bf176c7ef4c12d0b1d70bc57a57eaa1dfec61f
erikandersson96 <erik96.andersson@gmail.com>
2022-08-24 11:21:16 +0000

    add only slug as argument remove_favorite

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path(
         '', views.ProfileFavorite.as_view(),
         name='profile_favorite'),
+    path(
+        'remove_favorite/<slug:slug>', views.remove_favorite,
+        name='remove_favorite'),
     path('add_favorite/<slug:slug>', views.add_favorite, name='add_favorite'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.views import View, generic
+from django.core.exceptions import PermissionDenied
 
 
 class ProfileFavorite(generic.ListView):
@@ -17,13 +18,15 @@ class ProfileFavorite(generic.ListView):
 
 
 @login_required
-def remove_favorite(request, slug):
+def remove_favorite(slug):
     """
     Remove from favorite recipe list
     """
-    current_user = request.user
-    current_recipe = get_object_or_404(Recipe, slug=slug)
-    Favorite.objects.pop(user=current_user, favorite_recipe=current_recipe)
+    remove_user_favorite = Favorite.objects.get(slug=slug)
+    if remove_user_favorite.user == request.user:
+        remove_user_favorite.delete()
+    else:
+        raise PermissionDenied()
     return redirect('profile_favorite')
 
 

76d98a205da047b32a94f80eba62159f87cf62dc
erikandersson96 <erik96.andersson@gmail.com>
2022-08-24 17:12:56 +0000

    register models

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import Favorite
 
 
-@admin.register(Favorite)
 class FavoritesAdmin(admin.ModelAdmin):
     """
     Admin for favorite recipes
     """
     list_display = ('user', 'favorite_recipe')
+
+
+# Register the models
+admin.site.register(Favorite, FavoritesAdmin)
